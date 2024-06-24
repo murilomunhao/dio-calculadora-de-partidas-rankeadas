fde35ead43e61c41b44ca62497ff70dc8596a320
Murilo <contato@murilomunhao.com>
2024-06-23 23:52:49 -0300

    Projeto calculadora de partidas rankeadas

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# dio-calculadora-de-partidas-rankeadas
+# Calculadora de Partidas Ranqueadas
+
+Este é um projeto de uma calculadora simples para determinar o saldo de vitórias em partidas ranqueadas, exibindo o nível correspondente ao resultado.
+
+![calculadora](https://github.com/murilomunhao/dio-calculadora-de-partidas-rankeadas/assets/7818593/6f3e7783-96d7-4155-a5ad-e99b32885e79)
+
+## Funcionalidades
+
+- Calcula o saldo de vitórias subtraindo as derrotas das vitórias inseridas pelo usuário.
+- Exibe o nível de ranque correspondente ao saldo de vitórias calculado.
+- Possui um botão para limpar os campos de entrada.
+- Mostra notificações visuais para informar o usuário sobre o resultado do cálculo.
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Como Utilizar
+
+1. Clone o repositório:
+
+   ```bash
+   git clone https://github.com/seu-usuario/seu-repositorio.git
+
+Instruções de Uso
+
+2. Abra o arquivo `index.html` em seu navegador web.
+3. Insira o número de vitórias e derrotas nos campos de entrada.
+4. Clique no botão "Calcular" para ver o resultado.
+5. Caso deseje limpar os campos, clique no botão "Limpar".
+
+Acesse 
+
+Estrutura do Projeto
+
+- **index.html**: Contém a estrutura da página web e os elementos interativos.
+- **main.css**: Estiliza a página utilizando CSS, incluindo animações e layout responsivo.
+- **ranked.js**: Implementa a lógica de funcionamento da calculadora em JavaScript.
+
+Créditos
+
+- **Desenvolvido por**: Murilo Munhão
+- **Curso**: Bootcamp Santander 2024 - Criando Jogos com Godot
---
+++ src/assets/js/ranked.js
@@ -0,0 +1,153 @@
+document.addEventListener('DOMContentLoaded', function () {
+const alertSong = document.getElementById('alert-song');
+const botao = document.querySelector('.btn');
+const alert = document.querySelector('.alert');
+const alertTitle = document.getElementById('alert-title');
+const alertDesc = document.getElementById('alert-desc');
+const result = document.getElementById('result');
+const txtInputs = document.getElementById('text-inputs');
+const victory = document.getElementById('victory');
+const lowser = document.getElementById('lowser');
+const btnCleanDisplay = document.getElementById('limpar');
+
+
+
+var listenerBtn = [];
+//Number buttons
+listenerBtn.push(document.getElementById("num0"));
+listenerBtn.push(document.getElementById("num1"));
+listenerBtn.push(document.getElementById("num2"));
+listenerBtn.push(document.getElementById("num3"));
+listenerBtn.push(document.getElementById("num4"));
+listenerBtn.push(document.getElementById("num5"));
+listenerBtn.push(document.getElementById("num6"));
+listenerBtn.push(document.getElementById("num7"));
+listenerBtn.push(document.getElementById("num8"));
+listenerBtn.push(document.getElementById("num9"));
+
+
+btnCleanDisplay.onclick = function () {
+  victory.value = "";
+  lowser.value = "";
+  victory.focus();
+};
+
+
+for (var i = 0; i < listenerBtn.length; i++) {
+  listenerBtn[i].addEventListener("click", writeOnDisplay);
+}
+
+var selectedInput = null;
+victory.focus();
+if(victory.focus){
+  selectedInput = victory;
+}else{
+  selectedInput = lowser;
+}
+
+if (victory && lowser) {
+  
+  victory.addEventListener('focus', function() {
+    selectedInput = victory;
+    
+  });
+
+  lowser.addEventListener('focus', function() {
+    selectedInput = lowser;
+  });
+
+  for (var i = 0; i < listenerBtn.length; i++) {
+    listenerBtn[i].addEventListener("click", writeOnDisplay);
+  }
+
+  function writeOnDisplay() {
+    let lastDigit = this.textContent; 
+    if (selectedInput) {
+      selectedInput.value += lastDigit;
+    }
+  }
+}
+
+botao.addEventListener('click', () => {
+    if((victory || lowser).value != ""){
+      let ranked = 0;
+      victoryValue = victory.value;
+      lowserValue = lowser.value;
+      ranked = victoryValue - lowserValue;
+      console.log(ranked);
+
+      switch (true) {
+        case (ranked < 10):
+          rankLevel = 'Ferro!';
+          desc = `O Herói tem de saldo de <span class="highlight">${ranked} vitórias</span> e está no nível de <span class="rank">${rankLevel}!</span>`;
+          showAlert("Resultado!", desc, 5000);
+          break;
+          case (ranked >= 11 && ranked <= 20):
+            rankLevel = 'Bronze!';
+            desc = `O Herói tem de saldo de <span class="highlight">${ranked} vitórias</span> e está no nível de <span class="rank">${rankLevel}!</span>`;
+            showAlert("Resultado!", desc, 5000);
+            break;
+        case (ranked >= 21 && ranked <= 50):
+          rankLevel = 'Prata!';
+          desc = `O Herói tem de saldo de <span class="highlight">${ranked} vitórias</span> e está no nível de <span class="rank">${rankLevel}!</span>`;
+          showAlert("Resultado!", desc, 5000);
+            break;
+        case (ranked >= 51 && ranked <= 80):
+          rankLevel = 'Ouro!';
+          desc = `O Herói tem de saldo de <span class="highlight">${ranked} vitórias</span> e está no nível de <span class="rank">${rankLevel}!</span>`;
+          showAlert("Resultado!", desc, 5000);
+            break;
+        case (ranked >= 81 && ranked <= 90):
+          rankLevel = 'Diamante!';
+          desc = `O Herói tem de saldo de <span class="highlight">${ranked} vitórias</span> e está no nível de <span class="rank">${rankLevel}!</span>`;
+          showAlert("Resultado!", desc, 5000);
+            break;
+        case (ranked >= 91 && ranked <= 100):
+          rankLevel = 'Lendário!';
+          desc = `O Herói tem de saldo de <span class="highlight">${ranked} vitórias</span> e está no nível de <span class="rank">${rankLevel}!</span>`;
+          showAlert("Resultado!", desc, 5000);
+            break;
+        case (ranked >= 101):
+          rankLevel = 'Imortal!';
+          desc = `O Herói tem de saldo de <span class="highlight">${ranked} vitórias</span> e está no nível de <span class="rank">${rankLevel}!</span>`;
+          showAlert("Resultado!", desc, 5000);
+            break;
+        default:
+          desc = "Não foi possível calcular o saldo de vitórias!";
+          showAlert("Atenção!",desc);
+            break;
+      }
+
+
+
+    }else{
+      showAlert("Atenção!","Todos os campos são obrigatórios!", 3000);
+      alertSong.play();
+
+
+    }
+
+    function showAlert(title,desc,time) {
+      if (!alert.classList.contains('card-alert')) {
+        result.style.display = 'none';
+        txtInputs.style.display = 'none';
+        alert.style.display = 'block';
+        alert.classList.add('card-alert');
+        alertTitle.innerHTML = title;
+        alertDesc.innerHTML = desc;
+      }
+      
+      setTimeout(() => {
+        alert.style.display = 'none';
+        result.style.display = 'block';
+        txtInputs.style.display = 'flex';
+        victory.value = "";
+        lowser.value = "";
+        victory.focus();
+        alert.classList.remove('card-alert');
+      }, time);
+
+      
+    }
+});
+});
